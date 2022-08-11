428b887ed0dc164037e85ba6fe58ad5d97ed588e
IvanQin <yifanfdu@gmail.com>
2022-08-10 20:20:36 -0700

    Enable Pylint and apply fixes

--- src/node/i_node.py
+++ src/node/i_node.py
@@ -6,7 +6,9 @@ class INode(ABC):
         self.node_id = node_id
 
     @abstractmethod
-    def get_node_id(self)-> str : raise NotImplementedError()
+    def get_node_id(self)-> str :
+        raise NotImplementedError()
 
     @abstractmethod
-    def start(self): raise NotImplementedError()
+    def start(self):
+        raise NotImplementedError()
